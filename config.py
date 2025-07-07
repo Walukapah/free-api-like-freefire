78dcffd7ec91e4dff63742d2e5d702f7a1ab73d2
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-07-07 21:45:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 CONFIG = {
      "EUROPE": "europe_config.json",
-    "IND": "ind_config.json",
-    "BR": "br_config.json",
+     "IND": "ind_config.json",
+     "BR": "br_config.json",
+     "SG": "sg_config.json",
 
 }
